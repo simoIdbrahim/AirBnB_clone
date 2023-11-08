e3373dc479dde4c1ac2fed0f23c6c16d51672960
imadovetch <ibouderoua63@gmail.com>
2023-11-08 17:46:08 +0100

    small update adding the methods of the class filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+import json
+import os
 
 class FileStorage:
+
+	__file_path = "file.json"
+    __objects = {}
+
+	def all(self):
+        return self.__objects
+	new(self, obj):
+		pass
+	save(self):
+		pass
+	reload(self):
+		pass
