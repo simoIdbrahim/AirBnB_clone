0a2cec0436fc398bbfc58f0f3825ca393e949b20
imadovetch <ibouderoua63@gmail.com>
2023-11-09 14:12:26 +0100

    somer updates to task file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,32 +10,33 @@ class FileStorage:
 
 	def all(self):
         return self.__objects
-	new(self, obj):
+	def new(self, obj):
 		key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
-	save(self):
+	def save(self):
 		try:
             with open(self.__file_path, "r") as data_file:
-                data = json.load(data_file)
+                    data = json.load(data_file)
         except FileNotFoundError:
             data = {}
 
         serialized_objects = {}
         for obj_key, obj_value in self.__objects.items():
-            serialized_objects[obj_key] = obj_value.to_dict()
-
+                serialized_objects[obj_key] =obj_value.to_dict()
         data.update(serialized_objects)
 
         with open(self.__file_path, "w") as data_file:
             json.dump(data, data_file, indent=4)
-	reload(self):
+	def reload(self):
 		if self.__file_path is not None:
             try:
                 with open(self.__file_path, "r") as data_file:
                     json_data = json.load(data_file)
+
                     for key, value in json_data.items():
                         class_name, obj_id = key.split('.')
                         class_obj = globals()[class_name]
+                        
                         self.__objects[key] = class_obj(**value)
             except FileNotFoundError:
                 pass
